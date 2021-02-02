32790a7d35e11a15ad40ee128f32edd849845077
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-02-02 15:11:51 +0300

    Added movie list

---
+++ src/movies.js
@@ -0,0 +1,26 @@
+const movies = [
+  {
+    id: 1,
+    name: "Inception",
+  },
+  {
+    id: 2,
+    name: "Black Panther",
+  },
+  {
+    id: 3,
+    name: "Shrek",
+  },
+  {
+    id: 4,
+    name: "Monsters, Inc.",
+  },
+  {
+    id: 5,
+    name: "V for Vendetta",
+  },
+  {
+    id: 6,
+    name: "Harry Potter and the Goblet of Fire",
+  },
+];
